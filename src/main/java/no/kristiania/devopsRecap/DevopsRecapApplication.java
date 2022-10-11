fa37330639550025c3fcf455b0f25ccf7bbe1dbd
mojibhashemi <72493740+mojibhashemi@users.noreply.github.com>
2022-10-11 09:39:05 +0200

    Init commit with hello

---
+++ src/main/java/no/kristiania/devopsRecap/DevopsRecapApplication.java
@@ -0,0 +1,13 @@
+package no.kristiania.devopsRecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsRecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsRecapApplication.class, args);
+	}
+
+}
